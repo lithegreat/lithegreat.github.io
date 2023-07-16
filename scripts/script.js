b753821cfbf64ec39196346d3c078455a5dccf1a
Hengsheng <muzimichaed@gmail.com>
2023-07-16 22:57:28 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -22,6 +22,9 @@ const guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
 let resetButton;
+
+guessField.focus();
+
 function checkGuess() {
     const userGuess = Number(guessField.value);
     if (guessCount === 1) {
@@ -52,3 +55,35 @@ function checkGuess() {
     guessField.value = '';
     guessField.focus();
 }
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+  
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (const resetPara of resetParas) {
+      resetPara.textContent = '';
+    }
+  
+    resetButton.parentNode.removeChild(resetButton);
+  
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+  
+    lastResult.style.backgroundColor = 'white';
+  
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+  
